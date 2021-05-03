d353d358dc665fd0a218ac19112cd9b480448969
anlv-dev <uKsRWHGjNJzkr3P>
2021-05-03 16:18:48 +0700

    okies

---
+++ example1.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+from deepface.commons import functions
+
+from deepface import DeepFace
+
+model = DeepFace.build_model('Facenet')
+facial_img_paths = []
+
+for root,directory, files in os.walk('dataset'):
+    for file in files:
+        if '.jpg' in file:
+            facial_img_path.append(root + '/' + file )
+
+
+# proecess : detect and aling for each file
+
+instance = []
+for i in pd.tqdm(range(0, len(facial_img_paths))):
+    facial_img_path = facial_img_paths[i]
+
